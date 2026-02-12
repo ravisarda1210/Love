6400eff2784a5f6a19b3b8ab0f0a8fa400042855
ravisarda1210 <ravi.sarda@gmail.com>
2026-02-12 16:15:54 +0530

    Refactor event listeners and music controls

--- yes-script.js
+++ yes-script.js
@@ -1,23 +1,35 @@
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+const message = document.getElementById("message");
+const music = document.getElementById("music");
+const musicBtn = document.getElementById("musicBtn");
 
-function startSurprise(){
-  document.getElementById("startScreen").style.display="none";
-  document.getElementById("mainContent").style.display="block";
+let noTexts = [
+  "Are you sure? 🥺",
+  "Think again! ❤️",
+  "I will keep asking 😜",
+  "You can’t say no!",
+  "Try pressing YES 😍"
+];
 
-  const music=document.getElementById("bg-music");
+let count = 0;
+
+yesBtn.addEventListener("click", () => {
   music.play().catch(()=>{});
+  confetti({ particleCount: 200, spread: 70 });
+  message.innerHTML = "You just made me the happiest person alive! 💖";
+});
 
-  confetti({
-    particleCount:150,
-    spread:70,
-    origin:{y:0.6}
-  });
-}
+noBtn.addEventListener("mouseover", () => {
+  let x = Math.random() * 200 - 100;
+  let y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+  message.innerHTML = noTexts[count % noTexts.length];
+  count++;
+});
 
-function toggleMusic(){
-  const music=document.getElementById("bg-music");
-  if(music.paused){
-    music.play();
-  }else{
-    music.pause();
-  }
-}
+musicBtn.addEventListener("click",()=>{
+  if(music.paused) music.play();
+  else music.pause();
+});
+
